6effe677271ffe2555bcdbb9e23466c3d29bcbad
Rádler Ákos <akos.radler@gmail.com>
2022-01-19 18:42:52 +0100

    Update .d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,5 @@ interface ICombineMuiSx<T extends Theme> {
     condition: boolean;
     sx: SxProps<T>;
 }
-declare const sxCompose: (...sx: Array<ICombineMuiSx | SxProps<T>>) => SxProps<T>;
+declare const sxCompose: <T extends Theme>(...sx: Array<ICombineMuiSx | SxProps<T>>) => SxProps<T>;
 export default sxCompose;
